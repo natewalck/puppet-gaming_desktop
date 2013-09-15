38499cf624bfa0a76922426b40cc02656df8bca8
natewalck <nate@osxadm.in>
2013-09-15 19:49:36 -0400

    Add a bunch of apps that I always load on my desktop

--- manifests/applications.pp
+++ manifests/applications.pp
@@ -1,6 +1,48 @@
 class gaming_desktop::applications (
   ){
 
+  package { '1password' :
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => 'latest'
+  }  
+
+  package { '7zip' :
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => 'latest'
+  }  
+
+  package { 'dropbox' :
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => 'latest'
+  }  
+
+  package { 'mumble' :
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => 'latest'
+  }  
+
+  package { 'pidgin' :
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => 'latest'
+  }  
+
+  package { 'spotify' :
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => 'latest'
+  }  
+
+  package { 'steam' :
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => 'latest'
+  }  
+
   package { 'SublimeText3' :
     ensure          => installed,
     provider        => 'chocolatey',
